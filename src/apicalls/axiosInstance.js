824a2ec11419a22deb0d51d6d33f869df50d740d
karthikeyan13762 <karthikeya13762@gmail.com>
2025-01-17 18:38:36 +0530

    Backend url added in frontend

--- src/apicalls/axiosInstance.js
+++ src/apicalls/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //  axiosInstance as globalobject
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://project-name-backend-marcketplace.onrender.com",
   headers: {
     // "Content-Type": "application/json", // Default content type
     // this is the standerd way to sendingtheauthorizationto the backend
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // Backend server URL
+        target: "https://project-name-backend-marcketplace.onrender.com", // Backend server URL
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ""), // Optional: Removes '/api' prefix
       },
