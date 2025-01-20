f57375c7998a2c13f2484748797f07f9c1d40f8e
karthikeyan13762 <karthikeya13762@gmail.com>
2025-01-21 01:12:47 +0530

    loading issue fixed

--- src/apicalls/axiosInstance.js
+++ src/apicalls/axiosInstance.js
@@ -1,11 +1,32 @@
+// import axios from "axios";
+
+// //  axiosInstance as globalobject
+// export const axiosInstance = axios.create({
+//   baseURL: "https://project-name-backend-marcketplace.onrender.com",
+//   headers: {
+//     // "Content-Type": "application/json", // Default content type
+//     // this is the standerd way to sendingtheauthorizationto the backend
+//     Authorization: `Bearer ${localStorage.getItem("token")}`,
+//   },
+// });
+
 import axios from "axios";
 
-//  axiosInstance as globalobject
+// Create an axios instance
 export const axiosInstance = axios.create({
   baseURL: "https://project-name-backend-marcketplace.onrender.com",
-  headers: {
-    // "Content-Type": "application/json", // Default content type
-    // this is the standerd way to sendingtheauthorizationto the backend
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
-  },
 });
+
+// Adding request interceptor to include Authorization header dynamically
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`; // Add token dynamically
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
